5e9000c816ac925897d2588bf956c6c47c908559
Taruna_joshi <taruna.joshi@izenda.com>
2020-04-09 09:22:56 +0530

    This is demo

---
+++ Object Repository/ErrorInvaliduser.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>ErrorInvaliduser</name>
+   <tag></tag>
+   <elementGuidId>f59e5200-52d0-464b-ae80-60ec237c35e2</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(text(),'Incorrect email or password. Please try again')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
